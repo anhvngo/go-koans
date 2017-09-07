f994df4aa2c01ef20bab2c867285665434b57fbe
Anh Ngo <anh@pinterest.com>
2017-09-07 15:27:31 -0700

    first try

--- about_basics.go
+++ about_basics.go
@@ -1,24 +1,25 @@
 package go_koans
 
 func aboutBasics() {
+	__bool__ := true
 	assert(__bool__ == true)  // what is truth?
 	assert(__bool__ != false) // in it there is nothing false
 
-	var i int = __int__
+	var i int = 1
 	assert(i == 1.0000000000000000000000000000000000000) // precision is in the eye of the beholder
 
-	assert(5%2 == __int__)
-	assert(5*2 == __int__)
-	assert(5^2 == __int__)
+	assert(5%2 == 1)
+	assert(5*2 == 10)
+	assert(5^2 == 7)
 
 	var x int
-	assert(x == __int__) // zero values are valued in Go
+	assert(x == 0) // zero values are valued in Go
 
 	var f float32
-	assert(f == __float32__) // for types of all types
+	assert(f == 0) // for types of all types
 
 	var s string
-	assert(s == __string__) // both typical or atypical types
+	assert(s == "") // both typical or atypical types
 
 	var c struct {
 		x int
