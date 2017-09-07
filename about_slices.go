586b29bfe029463b2b8cb353ee6bca9f5e4e7a80
Anh Ngo <anh@pinterest.com>
2017-09-07 16:20:26 -0700

    slices,string,arrays

--- about_slices.go
+++ about_slices.go
@@ -6,8 +6,8 @@ func aboutSlices() {
 	assert(fruits[0] == __string__) // slices seem like arrays
 	assert(len(fruits) == __int__)  // in nearly all respects
 
-	tasty_fruits := fruits[1:3]           // we can even slice slices
-	assert(tasty_fruits[0] == __string__) // slices of slices also share the underlying data
+	tastyFruits := fruits[1:3]           // we can even slice slices
+	assert(tastyFruits[0] == __string__) // slices of slices also share the underlying data
 
 	pregnancy_slots := []string{"baby", "baby", "lemon"}
 	assert(cap(pregnancy_slots) == __int__) // the capacity is initially the length
