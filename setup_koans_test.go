73c13cd14a649e94d5e6cbb40df45fba5fe9e3f5
Anh Ngo <anh@pinterest.com>
2017-09-11 15:25:51 -0700

    new one to play with

--- setup_koans_test.go
+++ setup_koans_test.go
@@ -24,25 +24,25 @@ const (
 var __runner__ runner = nil
 
 func TestKoans(t *testing.T) {
-	aboutBasics()
+	/*aboutBasics()
 	aboutStrings()
 	aboutArrays()
 	aboutSlices()
 	aboutTypes()
 	aboutControlFlow()
-	aboutEnumeration()
+	aboutEnumeration() */
 	aboutAnonymousFunctions()
-	aboutVariadicFunctions()
-	aboutFiles()
+	/*aboutVariadicFunctions()
+	aboutFiles() */
 	aboutInterfaces()
-	aboutCommonInterfaces()
+	/*aboutCommonInterfaces()
 	aboutMaps()
 	aboutPointers()
 	aboutStructs()
 	aboutAllocation()
 	aboutChannels()
 	aboutConcurrency()
-	aboutPanics()
+	aboutPanics()*/
 
 	fmt.Printf("\n%c[32;1mYou won life. Good job.%c[0m\n\n", 27, 27)
 }
